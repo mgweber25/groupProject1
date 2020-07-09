8d997024686c44b4716a1528f64d204925835c14
mgweber25 <mike@mgweber.com>
2020-07-09 18:06:06 -0500

    add fetch request for charity api

--- assets/js/charityAPI.js
+++ assets/js/charityAPI.js
@@ -1,1 +1,11 @@
+//CharityAPI.js
 
+fetch(
+    'https://projects.propublica.org/nonprofits/api/v2'
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    });

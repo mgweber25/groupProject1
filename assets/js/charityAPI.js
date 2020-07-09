48aaeba54fb48cc07cc40e3ae6228921087f06fd
mgweber25 <mike@mgweber.com>
2020-07-09 18:58:37 -0500

    update charity API function

--- assets/js/charityAPI.js
+++ assets/js/charityAPI.js
@@ -1,11 +1,22 @@
 //CharityAPI.js
 
-fetch(
-    'https://projects.propublica.org/nonprofits/api/v2'
-  )
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data);
-    });
+function findZipCode() {
+    var searchTerm = document.querySelector('#zipcode').value;
+    // Create a variable to hold the value of rating
+   // var = document.querySelector('#rating').value;
+
+
+
+fetch('https://projects.propublica.org/nonprofits/api/v2/search.json?q=' +
+
+    zipCode)
+
+  .then(function(response) {
+    return response.json();
+  })
+
+  .then(function(response) {
+    console.log(response.data[0]);
+    var zipCode = document.querySelector('#zipcode');
+  });
+};

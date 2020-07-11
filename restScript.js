0c71a0661fd711cfa81bdb7e0ce50e5887cc8ba2
durganp1 <durganp1@gmail.com>
2020-07-11 11:37:06 -0500

    add pull rest by zip

---
+++ restScript.js
@@ -0,0 +1,55 @@
+
+
+var zipCodeEl = document.querySelector("#zipcode");
+var submitButtonEl = document.querySelector("#submit-button");
+var restResultsEl = document.querySelector("#rest-results");
+
+var getRestData = function(zip) {
+    var restApiUrl = "https://opentable.herokuapp.com/api/restaurants?zip=" + zip;
+    fetch(restApiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRest(data, zip);
+            });
+            }
+            else {
+                alert("Error: " + response.statusText);
+            }
+    })
+};
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var userZip = zipCodeEl.value.trim();
+    console.log(zipCodeEl.value);
+    if (userZip) {
+        getRestData(userZip);
+        zipCodeEl.value = "";
+    }
+    else {
+        alert("please enter a zip code")
+    }
+};
+
+submitButtonEl.addEventListener("click", formSubmitHandler);
+
+var displayRest = function(rest, restaurants) {
+    var restArr = rest.restaurants;
+    console.log(restArr);
+    if (restArr.length === 0) {
+        restResultsEl.textContent = "No Restaurants Listed for that Zip Code.";
+        return;
+    }
+    restResultsEl.textContent = "";
+    for (i=0; i<restArr.length; i++) {
+        var restInfo = restArr[i].name + "/" + restArr[i].address + "/" + restArr[i].city + restArr[i].area + "/" + restArr[i].phone;
+        console.log(restInfo);
+        var restList = document.createElement("li");
+        restList.classList = "rest-list";
+        restList.textContent = restInfo;
+        restResultsEl.appendChild(restList);
+
+    }
+}
+
+

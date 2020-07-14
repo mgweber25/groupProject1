d3f304bd5ef6278402b2fa95df7365f758a74178
BradD <bradleyeday@gmail.com>
2020-07-13 19:58:57 -0500

    added mission statement and restaurant generation styling

--- restScript.js
+++ restScript.js
@@ -42,10 +42,10 @@ var displayRest = function(rest, restaurants) {
     }
     restResultsEl.textContent = "";
     for (i=0; i<restArr.length; i++) {
-        var restInfo = restArr[i].name + "/" + restArr[i].address + "/" + restArr[i].city + restArr[i].area + "/" + restArr[i].phone;
+        var restInfo = restArr[i].name + " - " + restArr[i].address + " - " + restArr[i].city + restArr[i].area + " - " + restArr[i].phone;
         console.log(restInfo);
-        var restList = document.createElement("li");
-        restList.classList = "rest-list";
+        var restList = document.createElement("p");
+        restList.classList = "rest-list pure-u-1-" + restArr.length;
         restList.textContent = restInfo;
         restResultsEl.appendChild(restList);
 
